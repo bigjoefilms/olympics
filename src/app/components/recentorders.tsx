8333c4c8a6f77bbaeced30957d26a17dbd03b9a5
Bigjoe.js <98628310+bigjoefilms@users.noreply.github.com>
2024-07-14 22:20:12 +0100

    added

--- src/app/components/recentorders.tsx
+++ src/app/components/recentorders.tsx
@@ -27,7 +27,7 @@ const RecentOrders: React.FC<TransactionListProps> =   ({ searchAddress, numTx }
         const pubKey = new solanaWeb3.PublicKey(address);
         let transactionList = await solanaConnection.getSignaturesForAddress(pubKey, { limit: numTransactions });
      
-        setTransactions(transactionList.map(transaction => ({
+        setTransactions(transactionList.map((transaction: { signature: any; blockTime: any; confirmationStatus: any; }) => ({
          
           signature: transaction.signature,
           blockTime: transaction.blockTime,
@@ -45,7 +45,7 @@ const RecentOrders: React.FC<TransactionListProps> =   ({ searchAddress, numTx }
     <div className='w-full col-span-1 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white overflow-scroll'>
       <h1>Recent Tranfers</h1>
       <ul>
-      {transactions.map((transaction, index) => (
+      {transactions.map((transaction: Transaction, index: number) => (
           <li
           key={transaction.signature}    
                   className='bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 flex items-center cursor-pointer'
@@ -55,7 +55,7 @@ const RecentOrders: React.FC<TransactionListProps> =   ({ searchAddress, numTx }
             </div>
             <div className='pl-4'>
               
-              <p className='text-gray-800 text-sm' onClick={handleCopyToClipboard} >{copied ? 'Copied!' : transaction.signature.substring(0, 20)}</p>
+              <p className='text-gray-800 text-sm'  >{copied ? 'Copied!' : transaction.signature.substring(0, 20)}</p>
               <p className='text-gray-400 text-sm'>{transaction.blockTime}</p>
             </div>
             <p className='lg:flex md:hidden absolute right-6 text-sm p-[10px] bg-[#c4f9c3] rounded-[8px]'>{transaction.confirmationStatus}</p>
